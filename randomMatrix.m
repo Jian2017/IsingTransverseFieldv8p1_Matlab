eeca269e48a00c7df4c7279936240d7d4d94a2b9
K450E_Ubuntu <wangjian2015la@gmail.com>
2017-09-26 13:36:47 -0700

    eigenVectorLook.m add script to see rare magnetization

--- randomMatrix.m
+++ randomMatrix.m
@@ -12,13 +12,13 @@ for i=1:L
 end
 
 for i=1:L-1
-    M(i,i+1)=-0.5;  % 0.45 binaryR(0.99,1.8,0.0); % 
-    % M(i,i+1)=-binaryR(0.99,3.0,0.0);  
+%     M(i,i+1)=-0.5;  % 0.45 binaryR(0.99,1.8,0.0); % 
+    M(i,i+1)=-binaryR(0.25,4.0,0.4);  
 end
 
-for i=1:L-2
-    M(i,i+2)=-binaryR(0.4,0.6,0.5); %1.5;
-end
+% for i=1:L-2
+%     M(i,i+2)=-binaryR(0.4,0.6,0.5); %1.5;
+% end
 
 
     function outp=binaryR(hS,hL,pS)
